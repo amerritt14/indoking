f987e77a9220ddb577ec87912c8214c4d2966225
Andrew Merritt <amerritt14@liberty.edu>
2018-10-08 15:26:59 -0400

    Ensure that policy is checked on every controller method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  # Pundit validations. Ensures policy is checked on every controller
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 end
