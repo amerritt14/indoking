bb324526e0f8f61f1ef14142d27ab25f136e7e40
Andrew Merritt <amerritt14@liberty.edu>
2018-10-09 10:37:32 -0400

    Update migration

--- db/migrate/20181009105847_change_columns_on_products.rb
+++ db/migrate/20181009105847_change_columns_on_products.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class ChangeColumnsOnProducts < ActiveRecord::Migration[5.0]
-  def change
+  def up
     remove_column :products, :description
     remove_column :products, :thumbnail_url
     remove_column :products, :preview_url
 
     add_column :products, :variants, :integer
     add_column :products, :synced, :integer
+  end
+
+  def down
+    add_column :products, :description
+    add_column :products, :thumbnail_url
+    add_column :products, :preview_url
+
+    remove_column :products, :variants, :integer
+    remove_column :products, :synced, :integer
   end
 end
